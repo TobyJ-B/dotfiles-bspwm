78ad540e6e7509e36bcf13a1e5e5e6eced88c98d
TobyJ-B <tobybackstrom45@gmail.com>
2025-03-31 23:54:10 +0000

    added something idk

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -5,5 +5,11 @@ vim.g.mapleader = " "
 vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', opts) -- Toggle file explorer (NvimTree)
 vim.keymap.set('n', '<leader>f', ':NvimTreeFindFile<CR>', opts) -- Find file in NvimTree
 
+
+-- Yank to global clipboard
 vim.keymap.set("v", "<leader>y", '"+y', { noremap = true, silent = true })
+
+--Telescope Options
+
+vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { noremap = true, silent = true })
 
